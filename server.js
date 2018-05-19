1ddca4890611772086d840d9b480f045bcd1771b
Dennis Ruto <rutodeno@gmail.com>
2018-05-19 13:19:20 -0400

    added broadcast feature

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-var express = require("express");
-var app =  express();
+//var express = require("express");
+var app =  require("express")();
+var http = require("http").Server(app);
 var path = require("path");
-var io = require("socket.io")(app);
+var io = require("socket.io")(http);
 
 var PORT = process.env.PORT || 4000;
 
 app.get("/", function(req, res) {
 
-    res.sendfile(path.join(__dirname + "/display/index.html"))
+    res.sendFile(path.join(__dirname + "/display/index.html"))
 
 });
 
 io.on("connection", function(socket) {
     console.log("A user connected");
-})
 
 
-app.listen(PORT, function(){
+    socket.on("chat message", function(msg) {
+        io.emit("chat message", msg);
+        console.log("message: " +msg);
+    });
+
+    socket.on("disconnect", function() {
+        console.log("user disconnected");
+    });
+});
+
+http.listen(PORT, function(){
 
     console.log("listening on PORT "+PORT);
 });
-
-
-
-
